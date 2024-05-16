6dc6cd62d4e0b48198f013006b4fe4a90752a894
Mikhail Krichanov <mikhailkrichanov@gmail.com>
2024-05-16 15:41:46 +0300

    Ring3: Added EL0 and PAN support for AARCH64.

--- ArmPkg/Include/Chipset/AArch64.h
+++ ArmPkg/Include/Chipset/AArch64.h
@@ -26,6 +26,7 @@
 
 // ID_AA64MMFR1 - AArch64 Memory Model Feature Register 0 definitions
 #define AARCH64_MMFR1_VH  (0xF << 8)
+#define AARCH64_MMFR1_PAN (0xF << 20)
 
 // ID_AA64PFR0 - AArch64 Processor Feature Register 0 definitions
 #define AARCH64_PFR0_FP   (0xF << 16)
@@ -231,6 +232,16 @@ ArmReadHcr (
 
 UINTN
 ArmReadCurrentEL (
+  VOID
+  );
+
+VOID
+ArmSetPan (
+  VOID
+  );
+
+VOID
+ArmClearPan (
   VOID
   );
 

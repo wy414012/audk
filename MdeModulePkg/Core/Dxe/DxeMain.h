cd65706178053c7d942777aeb633794b8b9367c8
Mikhail Krichanov <mikhailkrichanov@gmail.com>
2025-01-16 15:43:34 +0300

    Ring3: Refactored out CoreRsp and UserStackTop.

--- MdeModulePkg/Core/Dxe/DxeMain.h
+++ MdeModulePkg/Core/Dxe/DxeMain.h
@@ -234,6 +234,16 @@ typedef struct {
   UINTN                                   UserStackTop;
 } LOADED_IMAGE_PRIVATE_DATA;
 
+typedef struct {
+  VOID        *CoreWrapper;
+  VOID        *UserSpaceDriver;
+  UINTN       UserPageTable;
+  UINTN       UserStackTop;
+  UINTN       SysCallStackTop;
+  UINTN       ReturnSP;
+  LIST_ENTRY  Link;
+} USER_SPACE_DRIVER;
+
 #define LOADED_IMAGE_PRIVATE_DATA_FROM_THIS(a) \
           CR(a, LOADED_IMAGE_PRIVATE_DATA, Info, LOADED_IMAGE_PRIVATE_DATA_SIGNATURE)
 
@@ -278,6 +288,7 @@ extern VOID                              *gRing3Interfaces;
 extern VOID                              *gRing3EntryPoint;
 extern UINTN                             gUserPageTable;
 extern UINTN                             gCorePageTable;
+extern LIST_ENTRY                        gUserSpaceDriversHead;
 
 //
 // Service Initialization Functions
@@ -2727,9 +2738,7 @@ EFI_STATUS
 EFIAPI
 CallBootService (
   IN UINT8  Type,
-  IN UINTN  *UserArguments,
-  IN UINTN  UserStackTop,
-  IN UINTN  SysCallStackTop
+  IN UINTN  *UserArguments
   );
 
 VOID
@@ -2747,10 +2756,9 @@ ForbidSupervisorAccessToUserMemory (
 EFI_STATUS
 EFIAPI
 GoToRing3 (
-  IN UINT8  Number,
-  IN VOID   *EntryPoint,
-  IN UINTN  UserStackTop,
-  IN UINTN  SysCallStackTop,
+  IN UINT8              Number,
+  IN VOID               *EntryPoint,
+  IN USER_SPACE_DRIVER  *UserDriver,
   ...
   );
 
